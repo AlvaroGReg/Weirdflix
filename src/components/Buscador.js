8195fdbea6ccddd8faec397ca41c57d9f524fb88
AlvaroGReg <alvaro.gracia.reg@gmail.com>
2023-05-30 10:22:55 +0200

    searcher

--- src/components/Buscador.js
+++ src/components/Buscador.js
@@ -3,20 +3,22 @@ import React, { useState } from "react"
 export const Buscador = ({listadoState, setListadoState}) => {
 
 	const [busquedaState, setBusquedaState] = useState('')
+	const [noEncontradoState, setNoEncontradoState] = useState(false)
 
 	const searchFilm = (e) => {
 		//Create state and refresh
-
 		setBusquedaState(e.target.value)
 
 		//Filter
 		let foundFilms = listadoState.filter( film => {
-			return film.titulo.toLocaleLowerCase().includes(busquedaState.toLocaleLowerCase)
+			return film.titulo.toLocaleLowerCase().includes(busquedaState.toLocaleLowerCase())
 		})
 
 		//Check results
 		if (busquedaState.length <= 1 || foundFilms <= 0) {
 			foundFilms = JSON.parse(localStorage.getItem('pelis'))
+
+			setNoEncontradoState(false)
 		}
 		
 		//Give value to localstorage
@@ -29,13 +31,15 @@ export const Buscador = ({listadoState, setListadoState}) => {
 	return (
 		<div className="search">
 			<h3 className="title">Buscador: {busquedaState}</h3>
+
+			{(noEncontradoState === true && busquedaState.length) && (<span className="no-encontrado">No se ha encontrado ninguna coincidencia</span>)}
+	
 			<form action="">
 				<input
 					type="text"
 					name="busqueda"
 					id="search_field"
 					autoComplete="off"
-					value="Lo que estoy buscando"
 					onChange={ searchFilm }
 				/>
 				<button id="search">Buscar</button>
